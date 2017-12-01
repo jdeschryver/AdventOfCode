dafa5946bf73a4096bb4dff625556f59ca2e6236
Jan De Schryver <Jan.DeSchryver@UGent.be>
2017-12-01 17:08:41 +0100

    NotQuiteLisp

--- src/main/kotlin/InverseCaptcha.kt
+++ src/main/kotlin/InverseCaptcha.kt
@@ -1,20 +1,16 @@
 /**
  * @author Jan De Schryver <Jan.DeSchryver@bucephalus.be>
  */
-// 1 december 2018
+// 1 december 2017
 
-fun decodeCaptcha(captcha: String) =
-        captcha.filterIndexed({i, c -> captcha[(i+1)%captcha.length] == c})
+fun decodeCaptcha(captcha: String, l: Int = captcha.length / 2) =
+        captcha.filterIndexed({i, c -> captcha[(i + l) % captcha.length] == c})
                 .sumBy { it.toRealInt() }
 
-fun decodeCaptcha2(captcha: String, l: Int = captcha.length/2) =
-        captcha.filterIndexed({i, c -> captcha[(i+l)%captcha.length] == c})
-                .sumBy { it.toRealInt() }
-
-fun Char.toRealInt() = this.toInt() - 48
+fun Char.toRealInt() = this - '0'
 
 fun main(args:Array<String>) {
     val captcha = "111233"
+    println(decodeCaptcha(captcha, 1))
     println(decodeCaptcha(captcha))
-    println(decodeCaptcha2(captcha))
 }
