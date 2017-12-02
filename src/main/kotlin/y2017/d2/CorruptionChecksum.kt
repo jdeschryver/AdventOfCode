07680df52ccc71275715cc8ea86213d9a9535756
Jan De Schryver <Jan.DeSchryver@UGent.be>
2017-12-02 16:39:39 +0100

    y2017d2 clean version

--- src/main/kotlin/y2017/d2/CorruptionChecksum.kt
+++ src/main/kotlin/y2017/d2/CorruptionChecksum.kt
@@ -1,39 +1,23 @@
-import y2015.calculatePaper
 import java.io.File
-import java.lang.Integer.MAX_VALUE
-import java.lang.Integer.MIN_VALUE
 
 /**
  * @author Jan De Schryver <Jan.DeSchryver@bucephalus.be>
  */
 
 
-fun minMaxLine(line: String) =
-    line.split(" ", "\t")
-            .map(String::toInt)
-            .fold( MAX_VALUE to MIN_VALUE){ acc, i ->  minOf(acc.first, i) to maxOf(acc.second, i)}
-            .difference()
+fun minMaxLine(line: List<Int>) = (line.max() ?: 0) - (line.min() ?: 0)
 
-fun Pair<Int, Int>.difference() = this.second - this.first
+fun divisibleLine(line: List<Int>) = line.asSequence().flatMap { i ->
+               line.asSequence().filter {i > it && i % it == 0 }.map { i / it }
+    }.first()
 
-fun divisibleLine(line: String): Int{
-    val ints = line.split(" ", "\t").map(String::toInt)
 
-    return ints.map { i -> ints.map { j -> i to j } }
-            .flatten()
-            .first { it.divisible() }.divide()
-}
-
-fun Pair<Int, Int>.divisible() =
-        this.second != this.first && (this.second % this.first == 0 || this.first % this.second == 0)
-
-fun Pair<Int, Int>.divide() = maxOf(this.first, this.second) / minOf(this.first, this.second)
 
 fun main(args: Array<String>) {
     val input = File( "src\\main\\kotlin\\y2017\\d2\\input.txt")
             .inputStream()
             .use { it.bufferedReader().readLines() }
 
-    println(input.sumBy { minMaxLine(it) })
-    println(input.sumBy { divisibleLine(it) })
+    println(input.sumBy { minMaxLine(it.split(" ", "\t").map(String::toInt)) })
+    println(input.sumBy { divisibleLine(it.split(" ", "\t").map(String::toInt)) })
 }
